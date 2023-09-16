13cccecb65095e875e67485e46081f42b39471c9
Caio Melo <105840564+CaioMelo10@users.noreply.github.com>
2023-09-16 00:37:05 -0300

    Update C3PO.java

--- robots/c3po/C3PO.java
+++ robots/c3po/C3PO.java
@@ -6,281 +6,79 @@ import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 public class C3PO extends Robot {
     
-   
-private int contador = 0;
+    private int contador = 0;
     private double girarArma = 10;
     private String nomeAlvo;
 
     public void run() {
-        setColors(Color.orange, Color.red, Color.yellow, Color.black, Color.orange);
-        nomeAlvo = 
-        setColors(Color.orange, Color.red, Color.yellow, Color.black, Color.orange);
-        nomeAlvo
+        setColors(Color.blue, Color.green, Color.magenta, Color.white, Color.pink);
+        nomeAlvo = null;
 
-        setColors(Color.orange, Color.red, Color.yellow, Color.black, Color.orange);
-       
-
-        setColors(Color.orange, Color.red, Color.yellow, Color
-
-        setColors(Color.orange, Color.red, Color.yellow,
-
-        setColors(Color.orange, Color.red
-
-        setColors(Color.orange
-null;
-        setAdjustGunForRobotTurn(
-        setAdjustGunForRobot
-
-        setAdjust
-true);
+        setAdjustGunForRobotTurn(true);
 
         while (true) {
             girarArmaPeriodicamente();
             contador++;
-            ahead(
-            girarArmaPeriodicamente();
-            contador++;
-           
-
-            girarArmaPeriodicamente();
-           
-
-            gir
-10);
-
-            if (contador > 11) {
-                nomeAlvo = 
-                nomeAlvo =
-
-                nomeAlvo
-
-               
-null;
-            }
+            scan();
         }
     }
 
-    
-            }
-        }
-    }
-
-private void girarArmaPeriodicamente() {
-        
-       
-if (contador > 2 && contador <= 5) {
-            girarArma = -
-            girarArma =
-
-            gir
-10;
+    private void girarArmaPeriodicamente() {
+        if (contador > 2 && contador <= 5) {
+            girarArma = -10;
         } else if (contador > 5) {
-            girarArma = 
-            girarArma =
-
-            girar
-10;
+            girarArma = 10;
         }
         turnGunRight(girarArma);
     }
 
-    
-        }
-        turnGunRight(girar
-
-        }
-        turn
-
-       
-public void onScannedRobot(ScannedRobotEvent e) {
+    public void onScannedRobot(ScannedRobotEvent e) {
         double distancia = e.getDistance();
-
         
-
-       
-if (nomeAlvo != null && !e.getName().equals(nomeAlvo)) {
-            return;
-        }
-        
-        }
-if (nomeAlvo == null) {
+        if (nomeAlvo == null || !e.getName().equals(nomeAlvo)) {
             nomeAlvo = e.getName();
         }
 
         girarArma = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
+        turnGunRight(girarArma);
 
-        
-            nomeAlvo = e.getName();
-        }
-
-        girarArma = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading
-
-            nomeAlvo = e.getName();
-        }
-
-        girarArma = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadar
-
-            nomeAlvo = e.getName();
-        }
-
-        girarArma = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getR
-
-            nomeAlvo = e.getName();
-        }
-
-        girarArma = normalRelativeAngleDegrees(e.getBearing() + (getHeading()
-
-            nomeAlvo = e.getName();
-        }
-
-        girarArma = normalRelativeAngleDegrees(e.getBearing() +
-
-            nomeAlvo = e.getName();
-        }
-
-        girarArma = normalRelativeAngleDegrees(e.getB
-
-            nomeAlvo = e.getName();
-        }
-
-        girarArma = normalRelative
-
-            nomeAlvo = e.getName();
-        }
-
-        girarArma
-
-            nomeAlvo = e.getName();
-        }
-
-        gir
-
-            nomeAlvo = e.getName();
-       
-
-            nomeAlvo
-
-            nome
-if (distancia > 150) {
-            moverParaAlvo(distancia - 
-            moverParaAlvo(distancia
-
-            moverParaAl
-140);
-        } 
-        }
-
-       
-else {
-            fire(
-            fire
-3);
+        if (distancia > 150) {
+            moverParaAlvo(distancia - 140);
+        } else {
+            fire(3);
             if (distancia < 100) {
                 evitarColisao(e.getBearing());
             }
         }
-        scan();
     }
 
-    
-                evitarColisao(e.getBearing());
-            }
-        }
-        scan
-
-                evitarColisao(e.getBearing());
-
-                evitarColis
-
-               
-private void moverParaAlvo(double distancia) {
+    private void moverParaAlvo(double distancia) {
         ahead(distancia);
     }
 
-    
-        ahead(distancia);
-    }
-
-   
-
-        ahead(dist
-
-       
-private void evitarColisao(double bearing) {
+    private void evitarColisao(double bearing) {
         if (bearing > -90 && bearing <= 90) {
             back(40);
-        } 
-        }
-
-       
-else {
-            ahead(
-            ahead
-40);
+        } else {
+            ahead(40);
         }
     }
 
     public void onHitRobot(HitRobotEvent e) {
-        nomeAlvo = e.getName();
         girarArma = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
         turnGunRight(girarArma);
-        fire(
-        nomeAlvo = e.getName();
-        girarArma = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
-        turnGunRight(girarArma);
-       
-
-        nomeAlvo = e.getName();
-        girarArma = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
-        turnGunRight(g
-
-        nomeAlvo = e.getName();
-        girarArma = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
-       
-
-        nomeAlvo = e.getName();
-        girarArma = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadar
-
-        nomeAlvo = e.getName();
-        girarArma = normalRelativeAngleDegrees(e.getBearing() + (getHeading() -
-
-        nomeAlvo = e.getName();
-        girarArma = normalRelativeAngleDegrees(e.getBearing() + (
-
-        nomeAlvo = e.getName();
-        girarArma = normalRelativeAngleDegrees(e.getB
-
-        nomeAlvo = e.getName();
-        girarArma =
-
-        nomeAlvo = e.getName();
-        girar
-
-        nomeAlvo = e.getName();
-
-        nomeAl
-3);
-        back(
-        back(
-50);
+        fire(3);
+        back(50);
     }
 
     public void onHitWall(HitWallEvent e) {
-        back(
-        back(
-50);
+        back(50);
         turnRight(90);
         ahead(35);
     }
 
     public void onHitByBullet(HitByBulletEvent e) {
-        turnRight(normalRelativeAngleDegrees(
-        turnRight(normalRelativeAngleDegrees(
-
-        turnRight(normal
-90 - (getHeading() - e.getHeading())));
+        turnRight(normalRelativeAngleDegrees(90 - (getHeading() - e.getHeading())));
     }
 }
 
-    }
-}
